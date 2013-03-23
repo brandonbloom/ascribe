de8c891aad944b348296ac2d4893a4a3a09b4fd4
Brandon Bloom <brandon@brandonbloom.name>
2013-03-23 13:09:43 -0400

    lein new ascribe

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ascribe
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/ascribe/core_test.clj
@@ -0,0 +1,7 @@
+(ns ascribe.core-test
+  (:use clojure.test
+        ascribe.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
