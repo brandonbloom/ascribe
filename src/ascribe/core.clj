857dc72052e23b90e26e8360971621cee8975f31
Brandon Bloom <brandon@brandonbloom.name>
2013-03-25 22:44:22 -0400

    Add attribute call tracing

--- src/ascribe/core.clj
+++ src/ascribe/core.clj
@@ -21,45 +21,53 @@
   (-root [_] root)
   (-path [_] path))
 
-;(defn attr-fn [name f]
-;  (fn [node]
-;    (assert (instance? NodeRef node))
-;    (f node)))
-;
-;(defmacro defattr [name args & body]
-;  `(def ~name (attr-fn ~(keyword name) (fn ~name ~args ~body))))
+(def ^:dynamic *depth* -1)
+
+(def ^:dynamic *trace* true)
+
+(defn attr-fn [kw f]
+  (fn [node]
+    (assert (instance? NodeRef node) "Attribute fn must be applied to a node")
+    (binding [*depth* (inc *depth*)]
+      (when *trace*
+        (print (apply str (repeat (* 2 *depth*) \space)))
+        (println kw "@" (-path node)))
+      (f node))))
+
+(defmacro defattr [name args & body]
+  `(def ~name (attr-fn ~(keyword name) (fn ~name ~args ~@body))))
 
 ;;; intrinsics
 
-(defn parent [node]
+(defattr parent [node]
   (let [p (-path node)]
     (when (seq p)
       (NodeRef. (-root node) (pop p)))))
 
-(defn root? [node]
+(defattr root? [node]
   (nil? (parent node)))
 
 ;;; repmin tree
 
-(defn value [node]
+(defattr value [node]
   (:value @node))
 
-(defn left [node]
+(defattr left [node]
   (NodeRef. (-root node) (conj (-path node) :left)))
 
-(defn right [node]
+(defattr right [node]
   (NodeRef. (-root node) (conj (-path node) :right)))
 
-(defn lmin [node]
+(defattr lmin [node]
   (or (value node)
       (min (-> node left lmin) (-> node right lmin))))
 
-(defn gmin [node]
+(defattr gmin [node]
   (if (root? node)
     (lmin node)
     (-> node parent gmin)))
 
-(defn ret [node]
+(defattr ret [node]
   (if (value node)
     {:value (gmin node)}
     {:left (-> node left ret) :right (-> node right ret)}))
